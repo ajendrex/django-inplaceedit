017d5cb008aaa2db36d0d8622d5925b3acda44fc
Héctor Urbina <hurbinas@gmail.com>
2015-04-10 08:22:18 -0300

    fix modelform_factory error

--- inplaceeditform/fields.py
+++ inplaceeditform/fields.py
@@ -106,7 +106,7 @@ class BaseAdaptorField(object):
         return config
 
     def get_form_class(self):
-        return modelform_factory(self.model)
+        return modelform_factory(self.model, exclude=())
 
     def get_form(self):
         form_class = self.get_form_class()
